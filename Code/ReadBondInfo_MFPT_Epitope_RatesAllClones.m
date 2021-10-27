003f4d116c6a520f8382176f557fca4aa9ad7891
amitaiassaf <amitaiassaf@gmail.com>
2021-10-26 21:54:26 -0700

    Update ReadBondInfo_MFPT_Epitope_RatesAllClones.m

--- Code/ReadBondInfo_MFPT_Epitope_RatesAllClones.m
+++ Code/ReadBondInfo_MFPT_Epitope_RatesAllClones.m
@@ -1,237 +1,190 @@
-%  Author :  Assaf Amitai
+%  Author:  Assaf Amitai
 %            amitaiassaf@gmail.com
 %            https://github.com/amitaiassaf/SpikeGeometry
 
 
-dt = 1e-7;
-BondsArr = [];
-Boundscount = 0;
-count = 1;
-epitope = 11;
+% The script computes the on-rates for the first and second antibody (Ab)
+% arm binding to the respective epitope using output (dump files) from
+% Lammps runs. The script further computes the probability of such
+% events.
 
-SpikesArr = [20];
+% To run the script put all the dump files in the run directory of the
+% script. The dump file should have the form:
+%  SpikeSim_idum_${idum}_virushaSpikes${SpikeN}Bond_Ep${epitopenum}_Cond${Cond}
+% ${idum}: seed number for the random number generator of Lammps.
+% ${SpikeN}: the number of spikes (HA molecules or S proteins) on the surface of the virus.
+% ${epitope}: the target residue (epitope) on the surface of HA. There is a total of 228 epitopes for HA
+% ${Cond}: a variable indicating different initial positions of the Ab.
 
-NArr = [40];
 
-epitopeArr = [7:234];
+% The output of the script is written to a matflie which contains the following variables
+% P1Arm: the probability of one Ab arm binding to the epitope
+% P2Arm: the probability of a second Ab arm binding to the epitope, given that the first is already bound.
+% kOn1Arm: the on-rate for the first arm binding.
+% kOn2Arm: the on-rate for the second arm binding, given that the first is already bound.
 
-Boundscount = [];
-BoundscountArr = zeros(length(NArr),length(epitopeArr));
-OneArmcountArr = zeros(length(NArr),length(epitopeArr));
-TwoArmcountArr = zeros(length(NArr),length(epitopeArr));
 
-KonArr = zeros(length(NArr),length(epitopeArr));
-Kon2ndArmArr = zeros(length(NArr),length(epitopeArr));
-KonArrCell = cell(length(NArr),length(epitopeArr));
-Kon2ndArmArrCell = cell(length(NArr),length(epitopeArr));
+dt = 1e-7; % conversion to the units of time. Based on Delta t chosen in the MD simulations
 
-BoundscountArrNorm = zeros(length(NArr),length(epitopeArr));
-BoundsTotcountArr = zeros(length(NArr),length(epitopeArr));
-BondEnergytArr = zeros(length(NArr),length(epitopeArr));
-BondDistArr = zeros(length(NArr),length(epitopeArr));
+SpikeN = 40;
 
-TwoArmDistBoundArr = zeros(length(NArr),length(epitopeArr));
-TwoArmDistBoundSameSpikeArr = zeros(length(NArr),length(epitopeArr));
-for n=1:length(NArr)
-    N = NArr(n);
+CondNum = 7; % The number of different initial configurations of the Ab with respect to the immunogen
 
-    SpikeNum = NArr(n);
-    for ep=1:length(epitopeArr)
+idumArr = [400:402] % idum span (seed to the random number generator of the simulation)
+
+epitopeArr = [7:234]; % All possible epitopes on HA (total of 228 epitopes)
+
+MFET_1stArm_Arr = zeros(1,length(epitopeArr)); % the mean first encounter time of 1 arm of the Ab to bind to its epitope
+MFET_2ndArm_Arr = zeros(1,length(epitopeArr)); % the mean first encounter time of both arms of the Ab to bind to their epitope
+FET_1stArm_ArrCell = cell(1,length(epitopeArr)); % cell array containing all the first encounter times of 1 arm of the Ab to bind to its epitope
+FET_2ndArm_ArrCell = cell(1,length(epitopeArr)); % cell array containing all the first encounter times of both arsm of the Ab to bind to their epitope
+SimNumEp = zeros(1,length(epitopeArr)); % number of simulations for each epitope
+
+for ep=1:length(epitopeArr)
+    
+    epitope = epitopeArr(ep);
+
+    FET_1stArm_epitope = [];
+    FET_2ndArm_epitope = [];
+
+    for cond=1:CondNum
         
-        epitope = epitopeArr(ep);
-        Boundscount = 0;
-        OneArmcount = 0;
-        TwoArmcount = 0;
-        TwoArmDistBoundSameSpike = 0;
-        KonN_epitope = [];
-        KonN_2ndArm_epitope = [];
-        BondEnergy = [];
-        BondDist = [];
-        TwoArmDistBound = [];
-        for cond=1:7
-
-            for idum =[400:402]
-                Kon = [];
-                Kon2ndarm = [];
-                
-
-                filename = ['SpikeSim_idum_',num2str(idum),'_virushaSpikes',num2str(SpikeNum),'_Bond_Ep_',num2str(epitope),'_Cond',num2str(cond)];
-                filename
-
-                if(exist(filename)==0)
-                    continue;
-                end
-                BoundscountArrNorm(n,ep) = BoundscountArrNorm(n,ep) + 1;
-                LatestArrvialtime = 0;
-                Prev1ArmMFPT = 0;
-                
-                data = readdump_all_bonds(filename);
-                BoundsTotcountArr(n,ep) = BoundsTotcountArr(n,ep) + length(data.timestep);
+        for idum =idumArr
+            TEF_1stArm_i = [];
+            TEF_2ndArm_i = [];
             
-                LatestMFPT = 0;
-                OneArmBindTime = [];
-                if(~isempty(find(data.BondNum)))
-                    Bondsidx = find(data.BondNum);
+            filename = ['SpikeSim_idum_',num2str(idum),'_virushaSpikes',num2str(SpikeN),'_Bond_Ep_',num2str(epitope),'_Cond',num2str(cond)];
+            filename
+            
+            if(exist(filename)==0)
+                continue;
+            end
+            
+            SimNumEp(1,ep) = SimNumEp(1,ep) + 1;
+            LatestArrvialtime = 0;
+            Prev1ArmMFPT = 0;
+            
+            data = readdump_all_bonds(filename);
+            
+            OneArmBindTime = [];
+            if(~isempty(find(data.BondNum)))
+                Bondsidx = find(data.BondNum);
+                
+                Target1 = [5 epitope];
+                Target2 = [epitope 5];
+                
+                for i=1:length(Bondsidx)
+                    bonds = data.Bonds{Bondsidx(i)};
+                    idx5 = find( (bonds(:,4)==5) | (bonds(:,5)==5) ); %% All attachments of Ab to epitope
+                    if(length(idx5) & isempty(TEF_1stArm_i))
+                        FET = dt*data.timestep(Bondsidx(i)); % The first encounter time of 1 Ab arm to target epitope
+                        TEF_1stArm_i =  FET;
+                        TEF_2ndArm_i = NaN;
+                    end
                     
-                    Target1 = [5 epitope];
-                    Target2 = [epitope 5];
-                 
-                    for i=1:length(Bondsidx)
-                        bonds = data.Bonds{Bondsidx(i)};
-                        idx5 = find( (bonds(:,4)==5) | (bonds(:,5)==5) ); %% All attachments of Ab to epitope
-                        if(length(idx5) & isempty(Kon))
-                            MFET = dt*data.timestep(Bondsidx(i));
-                            Kon =  MFET;
-                            Kon2ndarm = NaN;
-                        end
+                    Arm1idx = 0;
+                    Arm2idx = 0;
+                    SpikeArm1 = 0;
+                    SpikeArm2 = 0;
+                    
+                    OneArmBindTime = [OneArmBindTime ; data.timestep(Bondsidx(i)) ];
+                    for b=1:length(idx5)
+                        bond = bonds(idx5(b),:);
                         
-                        if(length(idx5) & (LatestMFPT==0)) % The lastest arrival time of one arm to the epitope
-                            LatestMFPT = dt*data.timestep(Bondsidx(i));
-                        end
-                        
-                        Arm1idx = 0;
-                        Arm2idx = 0;
-                        SpikeArm1 = 0;
-                        SpikeArm2 = 0;
+                        if( isequal(bond(4:5),Target1) | isequal(bond(4:5),Target2) );
 
-                        OneArmBindTime = [OneArmBindTime ; data.timestep(Bondsidx(i)) ];
-                        for b=1:length(idx5)
-                            bond = bonds(idx5(b),:);
-                            
-                            if( isequal(bond(4:5),Target1) | isequal(bond(4:5),Target2) );
-                                BondEnergy = [BondEnergy , bond(7)];
-                                BondDist = [BondDist , bond(6)];
-
-                                Position = find(bond(4:5)==5);
-                                EpitopePosition = find(bond(4:5)==epitope);
-                                idxArm = bond(Position+1);
-                                idxSpike = bond(EpitopePosition+1);
-                                if(Arm1idx==0)
-                                    Arm1idx = idxArm;
-                                    OneArmcount = OneArmcount+1;
-                                    SpikeArm1 = idxSpike;
-                                elseif(idxArm~=Arm1idx)
-                                    
-                                    if(isnan(Kon2ndarm))
-                                        
-                                        % find the latest time 1 Ab was
-                                        % bound
-                                        
-                                        d1ArmBound = diff(OneArmBindTime);
-                                        DTSim = min(d1ArmBound);
-                                        dtTime = 1.05*min(d1ArmBound);
-                                        d1ArmArrival = find(d1ArmBound>dtTime);
-                                        if(length(d1ArmArrival))
-                                            LatestArrvialtime = OneArmBindTime(d1ArmArrival(end)+1);
-                                        else
-                                            LatestArrvialtime = MFET;
-                                        end
-
-                                        MFET2arm = dt*(data.timestep(Bondsidx(i))-LatestArrvialtime);
-                                        if(MFET2arm==0)
-                                            MFET2arm = dt*DTSim;
-                                            Kon2ndarm = MFET2arm;
-                                        else
-                                            if(Prev1ArmMFPT~=LatestArrvialtime)
-                                                if(isnan(Kon2ndarm))
-                                                    Kon2ndarm = MFET2arm;
-                                                else
-                                                    Kon2ndarm = [Kon2ndarm ; MFET2arm];
-                                                end
-                                            end
-
-                                        end
-                                        Prev1ArmMFPT = LatestArrvialtime;
-                                    end
-
-                                    TwoArmcount = TwoArmcount+1;
-                                    
-                                    
-                                    
-                                end
+                            Position = find(bond(4:5)==5);
+                            EpitopePosition = find(bond(4:5)==epitope);
+                            idxArm = bond(Position+1);
+                            idxSpike = bond(EpitopePosition+1);
+                            if(Arm1idx==0)
+                                Arm1idx = idxArm;
+                                SpikeArm1 = idxSpike;
+                            elseif(idxArm~=Arm1idx)
                                 
-                                Boundscount = Boundscount+1;
+                                if(isnan(TEF_2ndArm_i))
+                                    
+                            
+                                    
+                                    d1ArmBound = diff(OneArmBindTime);
+                                    DTSim = min(d1ArmBound);
+                                    dtTime = 1.05*min(d1ArmBound);
+                                    d1ArmArrival = find(d1ArmBound>dtTime);
+                                    if(length(d1ArmArrival))
+                                        LatestArrvialtime = OneArmBindTime(d1ArmArrival(end)+1);
+                                    else
+                                        LatestArrvialtime = FET;
+                                    end
+                                    
+                                    FET2arm = dt*(data.timestep(Bondsidx(i))-LatestArrvialtime); % the time it takes the second Ab arm to bind once the first binds
+                                    if(FET2arm==0) % If the second arm binds at the same simlation step as the first arm impute the 2nd arm binding bind time as dt
+                                        FET2arm = dt*DTSim;
+                                        TEF_2ndArm_i = FET2arm;
+                                    else
+                                        if(Prev1ArmMFPT~=LatestArrvialtime)
+                                            if(isnan(TEF_2ndArm_i))
+                                                TEF_2ndArm_i = FET2arm;
+                                            else
+                                                TEF_2ndArm_i = [TEF_2ndArm_i ; FET2arm];
+                                            end
+                                        end
+                                        
+                                    end
+                                    Prev1ArmMFPT = LatestArrvialtime;
+                                end
 
                             end
+         
                         end
                     end
                 end
-                KonN_epitope = [KonN_epitope ; Kon];
-                KonN_2ndArm_epitope = [KonN_2ndArm_epitope ; Kon2ndarm];
-                
             end
-            end
-
-            KonArr(n,ep) = mean(KonN_epitope);
-            idxnotnan = find(~isnan(KonN_2ndArm_epitope));
-            Kon2ndArmArr(n,ep) = mean(KonN_2ndArm_epitope(idxnotnan));
+            FET_1stArm_epitope = [FET_1stArm_epitope ; TEF_1stArm_i];
+            FET_2ndArm_epitope = [FET_2ndArm_epitope ; TEF_2ndArm_i];
             
-            KonArrCell{n,ep} = KonN_epitope;
-            Kon2ndArmArrCell{n,ep} = KonN_2ndArm_epitope;
-            
-            BondEnergytArr(n,ep) = mean(BondEnergy);
-            BondDistArr(n,ep) = mean(BondDist);
-            TwoArmDistBoundArr(n,ep) = mean(TwoArmDistBound);
-            TwoArmDistBoundSameSpikeArr(n,ep) = TwoArmDistBoundSameSpike;
-
-            BoundscountArr(n,ep) = Boundscount/BoundsTotcountArr(n,ep);
-            OneArmcountArr(n,ep) = OneArmcount/BoundsTotcountArr(n,ep);
-            TwoArmcountArr(n,ep) = TwoArmcount/BoundsTotcountArr(n,ep);
-            
-            
-            
-        
-        
-    end
-end
-
-P1Arm = zeros(length(NArr),length(epitopeArr));
-P2Arm = zeros(length(NArr),length(epitopeArr));
-
-for i=1:length(NArr)
-    for j=1:length(epitopeArr)
-        OneArmMFPT = KonArrCell{i,j};
-        idxnotnan = find(~isnan(OneArmMFPT));
-        P1Arm(i,j) = length(idxnotnan)/BoundscountArrNorm(i,j);
-        
-        TwoArmMFPT = Kon2ndArmArrCell{i,j};
-        idxnotnan = find(~isnan(TwoArmMFPT));
-        P2Arm(i,j) = length(idxnotnan)/BoundscountArrNorm(i,j);
-        
-    end
-end
-
-kOn1Arm = zeros(length(NArr),length(epitopeArr));
-kOn2Arm = zeros(length(NArr),length(epitopeArr));
-
-
-for i=1:length(NArr)
-    for j=1:length(epitopeArr)
-        OneArmMFPT = KonArrCell{i,j};
-        idxnotnan = find(~isnan(OneArmMFPT));
-        if(length(idxnotnan))
-            kOn1Arm(i,j) = 1/mean(OneArmMFPT(idxnotnan));
-        end
-        
-        TwoArmMFPT = Kon2ndArmArrCell{i,j};
-        idxnotnan = find(~isnan(TwoArmMFPT));
-        if(length(idxnotnan))
-            kOn2Arm(i,j) = 1/mean(TwoArmMFPT(idxnotnan));
         end
     end
+    
+    MFET_1stArm_Arr(1,ep) = mean(FET_1stArm_epitope);
+    idxnotnan = find(~isnan(FET_2ndArm_epitope));
+    MFET_2ndArm_Arr(1,ep) = mean(FET_2ndArm_epitope(idxnotnan));
+    
+    FET_1stArm_ArrCell{1,ep} = FET_1stArm_epitope;
+    FET_2ndArm_ArrCell{1,ep} = FET_2ndArm_epitope;
+    
 end
 
-flag_str = ['virusha40Spikes'];
 
-kOn1Arm_HA = kOn1Arm;
-kOn2Arm_HA = kOn2Arm;
-OneArmcountArr_HA = OneArmcountArr;
-TwoArmcountArr_HA = TwoArmcountArr;
-P1Arm_HA = P1Arm;
-P2Arm_HA = P2Arm;
+P1Arm = zeros(1,length(epitopeArr)); % The fraction of simulations where 1 arm binds
+P2Arm = zeros(1,length(epitopeArr));% The fraction of simulations where 2 arms bind
 
-save([flag_str,'.mat'],'NArr','BoundscountArr','OneArmcountArr','TwoArmcountArr','KonArr','Kon2ndArmArr','KonArrCell',...
-    'Kon2ndArmArrCell','BoundscountArrNorm','BoundsTotcountArr','BondEnergytArr','BondDistArr','TwoArmDistBoundArr','TwoArmDistBoundSameSpikeArr',...
-    'P1Arm','P2Arm','kOn1Arm','kOn2Arm','P1Arm_HA','P2Arm_HA','kOn1Arm_HA','kOn2Arm_HA','OneArmcountArr_HA','TwoArmcountArr_HA','-v7.3');
+for j=1:length(epitopeArr)
+    OneArmMFPT = FET_1stArm_ArrCell{1,j};
+    idxnotnan = find(~isnan(OneArmMFPT));
+    P1Arm(1,j) = length(idxnotnan)/SimNumEp(1,j);
+    
+    TwoArmMFPT = FET_2ndArm_ArrCell{1,j};
+    idxnotnan = find(~isnan(TwoArmMFPT));
+    P2Arm(1,j) = length(idxnotnan)/SimNumEp(1,j);    
+end
 
+kOn1Arm = zeros(1,length(epitopeArr)); % The on-rate of the first Ab arm
+kOn2Arm = zeros(1,length(epitopeArr)); % The on-rate of the second arm of the Ab once the first arm binds
 
+for j=1:length(epitopeArr)
+    OneArmMFPT = FET_1stArm_ArrCell{1,j};
+    idxnotnan = find(~isnan(OneArmMFPT));
+    if(length(idxnotnan))
+        kOn1Arm(1,j) = 1/mean(OneArmMFPT(idxnotnan));
+    end
+    
+    TwoArmMFPT = FET_2ndArm_ArrCell{1,j};
+    idxnotnan = find(~isnan(TwoArmMFPT));
+    if(length(idxnotnan))
+        kOn2Arm(1,j) = 1/mean(TwoArmMFPT(idxnotnan));
+    end
+end
+ 
+flag_str = ['virusha',num2str(SpikeN),'Spikes'];
+
+save([flag_str,'.mat'],'P1Arm','P2Arm','kOn1Arm','kOn2Arm','-v7.3');
